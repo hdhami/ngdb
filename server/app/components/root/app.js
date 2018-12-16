ba4b291d004d6cb7e893e8b6dc61b6953555537c
LL\hardhami <hardhami@publicisgroupe.net>
2018-12-16 11:09:51 +0530

    separate git repo for dashboard

---
+++ server/app/components/root/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Dashboard App</div>;
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { green } from 'chalk';
+import App from './app/components/root/app';
+import Html from './app/html/dashboard';
+import PORT from './config/server-config';
+
+const server = express();
+
+server.get('/', (req, res) => {
+    const body = renderToString(<App />);
+    const title = 'Server side Rendering with Styled Components';
+
+    res.send(
+        Html({
+            body,
+            title
+        })
+    );
+});
+
+server.listen(PORT);
+green(`Serving running at http://localhost:${PORT}`);
