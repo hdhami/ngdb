7b6b9919a17c0f67eda6b04c12f1e0554e53768b
hdhami <hsd.1989@gmail.com>
2019-01-27 17:48:12 +0530

    master

--- app_modules/compiler/webpack/build-config.js
+++ app_modules/compiler/webpack/build-config.js
@@ -1,13 +1,11 @@
-const config = () => {
-    return {
-        paths: {
-            rootPathRelativeToCompiler: '../../../',
-            outputBasePath: '../../../gen/',
-            publicPath: '/',
-            componentsPathFromRoot: '/app/components/'
-        },
-        PORT: 9000
-    };
-};
+const config = () => ({
+    paths: {
+        rootPathRelativeToCompiler: '../../../',
+        outputBasePath: '../../../gen/',
+        publicPath: '/',
+        componentsPathFromRoot: '/app/components/'
+    },
+    PORT: 9000
+});
 
 export default config;
--- app_modules/compiler/webpack/entry-builder.js
+++ app_modules/compiler/webpack/entry-builder.js
@@ -4,17 +4,13 @@ const getVendorEntry = isProduction => {
         vendor: isProduction ? vendors : vendors.unshift('babel-polyfill')
     };
 };
-const getAppEntries = (path, rootPath) => {
-    return {
-        server: path.join(rootPath, 'app/server/server.js')
-    };
-};
+const getAppEntries = (path, rootPath) => ({
+    server: path.join(rootPath, 'app/server/server.js')
+});
 
-const getEntries = (isProduction, path, rootPath) => {
-    return {
-        ...getVendorEntry(isProduction),
-        ...getAppEntries(path, rootPath)
-    };
-};
+const getEntries = (isProduction, path, rootPath) => ({
+    ...getVendorEntry(isProduction),
+    ...getAppEntries(path, rootPath)
+});
 
 export { getEntries, getAppEntries, getVendorEntry };
