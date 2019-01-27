4fa5bb4bf9dc43a8445abf9187601525d4e64e60
hdhami <hsd.1989@gmail.com>
2019-01-27 21:08:43 +0530

    babel7 upgrade

--- app/server/server.js
+++ app/server/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-// import { green } from 'chalk';
 import App from '../components/root/app';
 import Html from '../html/dashboard';
 import { PORT } from '../config/server-config';
@@ -21,4 +20,5 @@ server.get('/', (req, res) => {
 });
 
 server.listen(PORT);
-// green(`Serving running at http://localhost:${PORT}`);
+// eslint-disable-next-line no-console
+console.log(`Serving running at http://localhost:${PORT}`);
